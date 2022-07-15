9cdc21d4b4da761f82708f3141f40e2337b20528
mayank agrawal <mayank.agrawal@unthinkable.co>
2022-07-15 21:53:19 +0530

    Create DB Models For BOOKS, ORDERS, LINE_ITEMS

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from books.models import Book, Order, OrderLineItem
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Order)
+admin.site.register(OrderLineItem)
